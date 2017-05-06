a88730353ec1930764faad4503cb5e41f10dbec3
long tang <long tang>
2017-05-06 12:47:17 +0800

    finish part one

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
-	if request.method == 'POST':
-		Item.objects.create(text = request.POST['item_text'])
-		return redirect('/')
-		
-	items = Item.objects.all()
-	return render(request, 'home.html', {'items': items})
+	return render(request, 'home.html')
+
+def view_list(request, list_id):
+	_list = List.objects.get(id = list_id)
+	return render(request, 'list.html', {'list': _list})
+
+def new_list(request):
+	_list = List.objects.create()
+	Item.objects.create(text = request.POST['item_text'], list = _list)
+	return redirect('/lists/%d/' % (_list.id,))
+
+def add_item(request, list_id):
+	_list = List.objects.get(id = list_id)
+	Item.objects.create(text=request.POST['item_text'], list = _list)
+	return redirect('/lists/%d/' % (_list.id,))
